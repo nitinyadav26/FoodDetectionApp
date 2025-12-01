3c2538b8f210bd8bef069dfec8543067c17c7e66
Nitin yadav <141497589+nitinyadav26@users.noreply.github.com>
2025-12-01 06:17:36 +0530

    Initial Commit

---
+++ FoodDetectionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodDetectionApp
+//
+//  Created by Nitin yadav on 01/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodDetectionApp/FoodDetectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDetectionAppApp.swift
+//  FoodDetectionApp
+//
+//  Created by Nitin yadav on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodDetectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
